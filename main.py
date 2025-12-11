6f884d48d9e60eb48cac821bf45059b4d2bfc78c
bluep <blue.phoenix10013@gmail.com>
2025-12-11 22:02:43 +0300

    onesignal automated test endpoint

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI,Request
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+from dotenv import load_dotenv
+import os
+load_dotenv()
+appId=os.getenv('ONESIGNAL_APP_ID')
+appRestKey=os.getenv('ONESIGNAL_APP_REST_KEY')
+appUrl=os.getenv('ONESIGNAL_APP_URL')
+app = FastAPI(title="OneSignal Notifications for MealMind Database")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+)
+
+@app.post('/orderAdded')
+async def orderAdded(request:Request):
+            order=await request.json()
+            print(f'order received : {order}')
