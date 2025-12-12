d5dc59a42517236a06ca9623f3e49c1c0461dfc4
bluep <blue.phoenix10013@gmail.com>
2025-12-12 11:33:09 +0300

    full code testing

--- main.py
+++ main.py
@@ -15,11 +15,38 @@ app.add_middleware(
     allow_methods=["*"],
 )
 
-@app.post('/orderAdded')
-async def orderAdded(request:Request):
-            order=await request.json()
-            print(f'order received : {order}')
-            print(f'{order.get("records")}')
 
-            return order
+
+@app.post("/orderAdded")
+async def handle_db_event(request: Request):
+    payload = await request.json()
+
+    # Paste this if you want to see the structure
+    print("DB Event:", payload)
+
+    record = payload.get("record", {})
+
+    user_id = record.get("userId")
+    send_low_stock_notification(user_id)
+
+    return {"status": "ok"}
+
+
+def send_low_stock_notification(user_id):
+    url = "https://api.onesignal.com/notifications"
+
+    body = {
+        "app_id": appId,
+        "include_external_user_ids": [str(user_id)],
+        "contents": {"en": f"New Order Placed ."},
+    }
+
+    headers = {
+        "Authorization": f"Basic {appRestKey}",
+        "Content-Type": "application/json",
+    }
+
+    res = requests.post(url, json=body, headers=headers)
+    print("OneSignal Response:", res.json())
+
 
